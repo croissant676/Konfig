e58986d0322a7abc084ef891030a78804a95dc57
Croissant676 <croissant676@outlook.com>
2022-08-31 19:39:03 -0500

    Starting from nothing

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+}
+
+group = "dev.kason"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/main/kotlin/dev/kason/konfig/Config.kt
@@ -0,0 +1,5 @@
+package dev.kason.konfig
+
+class Config {
+    
+}
